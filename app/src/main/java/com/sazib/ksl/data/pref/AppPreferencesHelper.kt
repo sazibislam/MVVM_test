08d136849c2a1f26311dc2f326e8cb9b491501e9
sazib <sazibislam1@gmail.com>
2021-10-01 08:42:06 +0600

    preference fixed

--- app/src/main/java/com/sazib/ksl/data/pref/AppPreferencesHelper.kt
+++ app/src/main/java/com/sazib/ksl/data/pref/AppPreferencesHelper.kt
@@ -1,16 +1,28 @@
 package com.sazib.ksl.data.pref
 
+import android.content.Context
 import android.content.SharedPreferences
+import com.sazib.ksl.di.ApplicationContext
+import com.sazib.ksl.di.PreferenceInfo
 import com.sazib.ksl.utils.AppConstants
+import com.sazib.ksl.utils.AppConstants.PREF_NAME
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class AppPreferencesHelper @Inject constructor(private val mPrefs: SharedPreferences) :
-    PreferencesHelper {
+@Singleton
+class AppPreferencesHelper @Inject constructor(
+    @ApplicationContext private val context: Context,
+    @PreferenceInfo prefName: String = PREF_NAME
+) : PreferencesHelper {
 
   companion object {
     private const val PREF_KEY_ACCESS_TOKEN = "PREF_KEY_ACCESS_TOKEN"
     private const val PREF_KEY_CURRENT_USER_ID = "PREF_KEY_CURRENT_USER_ID"
   }
+
+    private val mPrefs: SharedPreferences = context.getSharedPreferences(
+        prefName, Context.MODE_PRIVATE
+    )
 
   override fun getAccessToken(): String? {
     return mPrefs.getString(PREF_KEY_ACCESS_TOKEN, null)
